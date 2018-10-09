6cea4b335564eea6097540abc05ab366f476e339
Глухманюк Максим Александрович <MGNeo@yandex.ru>
2018-10-09 09:40:29 +0400

    Delete recharge.c

--- recharge.c
+++
@@ -1,8 +0,0 @@
-#include "recharge.h"
-#include "tank.h"
-#include "player.h"
-#include "enemies.h"
-#include "crash.h"
-
-static void recharge_tank(tank *const _tank,
-                          const float _dt);
